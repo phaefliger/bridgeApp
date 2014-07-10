f2a62db4541e0b696beee7394b13dfa9e37a8b4e
Paul Haefliger <paul.haefliger@my.wheaton.edu>
2014-07-10 16:52:38 -0400

    Initial Commit

---
+++ BridgeGame4/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BridgeGame4
+//
+//  Created by Paul Haefliger on 7/10/14.
+//  Copyright (c) 2014 Paul Haefliger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
